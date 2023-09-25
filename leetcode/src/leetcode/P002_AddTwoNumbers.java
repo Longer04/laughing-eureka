8ef7edd265dd575f24afdd4db4da911eeb9fa004
Longin <longin.koziolkiewicz@gmail.com>
2023-09-25 22:49:37 +0200

    [LEETCODE-002] AddTwoNumbers problem initial commit.

---
+++ leetcode/src/leetcode/P002_AddTwoNumbers.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @see <a href="https://leetcode.com/problems/add-two-numbers/description/">Add Two Numbers</a>
+ */
+public class P002_AddTwoNumbers {
+
+    public static void main(String... args) {
+
+
+    }
+
+    public static int[] getResult(int[] integers, int target) {
+        return null;
+    }
+}
