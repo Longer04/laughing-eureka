115a5b48946b146442fb40a51b39e514dd6023fc
Longin <longin.koziolkiewicz@gmail.com>
2023-10-07 11:56:29 +0200

    [LEETCODE-0020] added solution.

--- leetcode/src/leetcode/P0020_ValidParentheses.java
+++ leetcode/src/leetcode/P0020_ValidParentheses.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.Stack;
+
 /**
  * @see <a href="https://leetcode.com/problems/valid-parentheses/">Valid Parentheses</a>
  */
@@ -7,18 +9,34 @@ public class P0020_ValidParentheses {
 
     public static void main(String... args) {
 
-        String valid = "()[]{}";
-        String invalid = "()[{}";
+        final String valid = "()[]{}";
+        final String invalid = "()[{}";
 
-        boolean validResult = getResult(valid);
-        boolean invalidResult = getResult(invalid);
+        final boolean validResult = getResult(valid);
+        final boolean invalidResult = getResult(invalid);
 
         System.out.println(validResult);
         System.out.println(invalidResult);
     }
 
+    // Time = O(n), Space = O(n) - worst case whole string is stored in stack.
     private static boolean getResult(final String input) {
-
-        return false;
+        final Stack<Character> stack = new Stack<>();
+        for (char c : input.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                final Character element = stack.pop();
+                if ((c == ')' && element != '(') ||
+                        (c == ']' && element != '[') ||
+                        (c == '}' && element != '{')) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
     }
 }
