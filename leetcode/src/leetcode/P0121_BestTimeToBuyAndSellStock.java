7d097a42603d2731dba1b51fb8dbaf3aa8e4b51a
Longin <longin.koziolkiewicz@gmail.com>
2023-10-09 20:57:36 +0200

    [LEETCODE-0121] added problem.

---
+++ leetcode/src/leetcode/P0121_BestTimeToBuyAndSellStock.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+/**
+ * @see <a href="https://leetcode.com/problems/best-time-to-buy-and-sell-stock/">
+ * Best Time to Buy and Sell Stock</a>
+ */
+public class P0121_BestTimeToBuyAndSellStock {
+
+    public static void main(String... args) {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+
+        int result = getResult(prices);
+
+        System.out.println(result);
+    }
+
+    public static int getResult(int[] prices) {
+        return 0;
+    }
+}
