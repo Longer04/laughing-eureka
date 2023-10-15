add3e6e1aa483af5d4bf1bb6366c31bef71fbc9b
Longin <longin.koziolkiewicz@gmail.com>
2023-10-15 22:10:24 +0200

    [LEETCODE-0014].

--- leetcode/src/leetcode/P0014_LongestCommonPrefix.java
+++ leetcode/src/leetcode/P0014_LongestCommonPrefix.java
@@ -6,14 +6,14 @@ package leetcode;
 public class P0014_LongestCommonPrefix {
 
     public static void main(String... args) {
-        var val = 2;
+        String[] strings = {"flower", "flow", "flight"};
 
-        int result = getResult(val);
+        String result = getResult(strings);
 
         System.out.println(result);
     }
 
-    public static int getResult(int val) {
-        return 0;
+    public static String getResult(String[] val) {
+        return "";
     }
 }
