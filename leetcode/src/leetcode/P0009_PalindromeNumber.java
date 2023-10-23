a1cdfc139bfbc1a0fd9629205e46cdc7c5482909
Longin <longin.koziolkiewicz@gmail.com>
2023-10-23 21:52:37 +0200

    [LEETCODE-0009].

---
+++ leetcode/src/leetcode/P0009_PalindromeNumber.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+/**
+ * @see <a href="https://leetcode.com/problems/palindrome-number/">Palindrome Number</a>
+ */
+public class P0009_PalindromeNumber {
+
+    public static void main(String... args) {
+        int valid = 121;
+        int invalid = 123;
+
+        boolean result1 = getResult(valid);
+        boolean result2 = getResult(invalid);
+
+
+        System.out.println(result1);
+        System.out.println(result2);
+
+    }
+
+    public static boolean getResult(int val) {
+        return false;
+    }
+}
