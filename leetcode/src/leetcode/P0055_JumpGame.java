5e3be4e7f7479fcfabb7cd58339b61075ae12112
Longin <longin.koziolkiewicz@gmail.com>
2024-08-14 15:11:41 +0200

    [LEETCODE-0055] add problem.

---
+++ leetcode/src/leetcode/P0055_JumpGame.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+/**
+ * @see <a href="https://leetcode.com/problems/jump-game/description/">
+ * Jump Game</a>
+ */
+public class P0055_JumpGame {
+
+    public static void main(String... args) {
+        int[] input = {2,3,1,1,4};
+
+        boolean result = getResult(input);
+
+        System.out.println(result);
+    }
+
+    public static boolean getResult(int[] numbers) {
+
+        return false;
+    }
+}
